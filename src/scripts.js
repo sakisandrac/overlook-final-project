100ce7d0230c84edadc0b4e21da26adac71aeafe
“Saki <sakisandra.c@gmail.com>
2023-05-07 23:06:58 -0600

    Render cards for available rooms

--- src/scripts.js
+++ src/scripts.js
@@ -4,7 +4,10 @@ import './images/user.png';
 import './images/home.png';
 import './images/heart-logo.png';
 import './images/search.png';
-import './images/room1.jpg';
+import './images/single room.jpg';
+import './images/suite.jpg';
+import './images/residential suite.jpg';
+import './images/junior suite.jpg';
 
 const getBookings = (searchDate) => {
   fetch('http://localhost:3001/api/v1/bookings')
@@ -13,29 +16,23 @@ const getBookings = (searchDate) => {
     let dates = data.bookings.filter((booking) => {
       return booking.date === searchDate;
     })
-    if(dates.length !== 0) {
-    console.log(dates)
     renderBookings(dates);
-    } else {
-      console.log('No bookings found');
-    }
   })
   .catch(err => console.log(err));
 }
 
-getBookings('2022/02/20');
-
+// getBookings('2022/02/20');
 
 const getRooms = () => {
-  fetch('http://localhost:3001/api/v1/rooms')
+  return fetch('http://localhost:3001/api/v1/rooms')
   .then(res => res.json())
   .then(data => {
-    console.log(data);
+    console.log('all rooms in getRooms', data)
+    return data;
   })
   .catch(err => console.log(err));
 }
 
-getRooms()
 
 const getCustomers = () => {
   fetch('http://localhost:3001/api/v1/customers')
@@ -68,21 +65,61 @@ const toDashboard = () => {
 
 const searchBookings = () => {
   let date = searchDates.value.replaceAll('-', '/');
-  getBookings(date);
+  if(date) {
+    getBookings(date);
+  } else {
+    results.innerHTML = `<h2>Please select a date to search!</h2>`
+  }
 }
 
-const matchRooms = (rooms, booking) => {
-  return rooms.filter((room) => {
-    booking.forEach((book) => {
+const matchRooms = (bookedRooms) => {
+  // let availableRooms = []
+  let availableRooms = getRooms().then((res) => {
+    bookedRooms.forEach((booking) => {
+      for (let i=0; i< res.rooms.length; i++) {
+        if (booking.roomNumber === res.rooms[i].number){
+          res.rooms.splice(i, 1)
+        }
+      }
+    })
+    //DELETE BELOW
+    // res.rooms.forEach((room) => {
+    //   availableRooms.push(room)
+    // })
+    // console.log('available here', availableRooms)
+    // let newRooms = [...availableRooms]
+    // console.log('new', newRooms)
+    // return newRooms
+    return res.rooms;
+  })
+  // console.log('here in matchRooms', availableRooms)
+  console.log(availableRooms)
+  return availableRooms;
+  // return availableRooms
+}
+
+const renderBookings = (dates) => {
+    results.innerHTML = '';
+    console.log('dates in renderBookings', dates)
+    matchRooms(dates).then((res) => {
+      console.log('in renderBookings - avail rooms', res)
+      res.forEach((booking) => {
+
+        results.innerHTML += `<article class="card">
+        <img src="./images/${booking.roomType}.jpg" class="card-img">
+        <div class="card-text-wrapper">
+          <p class="card-booking-text">Room Number: ${booking.number}</p>
+          <p class="card-booking-text">Cost: $${booking.costPerNight
+          }</p>
+          <p class="card-booking-text">Room Type: ${booking.roomType}</p>
+          <p class="card-booking-text">Beds: ${booking.numBeds} ${booking.bedSize} sized bed</p>
+        </div>
+      </article>` 
+      })
       
     })
-  })
 }
-const renderBookings = (data) => {
-  data.forEach((booking) => {
-  results.innerHTML = `<h2>${booking.roomNumber}</h2>`
-  })
-}
+
 // EVENT LISTENERS
 newBookingNav.addEventListener('click', newBooking);
 dashboardNav.addEventListener('click', toDashboard);
