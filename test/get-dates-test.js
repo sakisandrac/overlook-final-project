1de0227813e151841754969b308f8520d0d2902f
“Saki <sakisandra.c@gmail.com>
2023-06-12 20:14:38 -0600

    Fix date testing

--- test/get-dates-test.js
+++ test/get-dates-test.js
@@ -9,17 +9,29 @@ describe('Get Dates', () => {
     it('should return the month in double digit form', () => {
       expect(setMonth(today).length).to.equal(2);
     });
+
+    it('should return the month as a string', () => {
+      expect(setMonth(today)).to.be.a('string');
+    });
   });
 
   describe('setDay', () => {
     it('should return the day in double digit form', () => {
-      expect(setDay(today).toString().length).to.equal(2);
+      expect(setDay(today).length).to.equal(2);
+    });
+
+    it('should return the day as a string', () => {
+      expect(setDay(today)).to.be.a('string');
     });
   });
 
   describe('getDate', () => {
     it('should return the date with 8 digits', () => {
       expect(getDate(today).length).to.equal(8);
+    });
+
+    it('should return the date as a string', () => {
+      expect(getDate(today)).to.be.a('string');
     });
   });
  })
